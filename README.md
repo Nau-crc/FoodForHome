90000bce4bd72127c823a75147ee14b3de448fe1
Laura <laura.rodcarrrion@gmail.com>
2021-01-21 11:19:49 +0100

    upcomming project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# FoodForHome
+# FoodForHome :apple:
+Applicacion Movil para tramitar pedidos de comida desde casa
+
+## Lenguajes :books:
+
+- HTML
+- CSS
+- JavaScript
+
+## Intension del proyecto :light:
+
+Ayudar al restaurante mas cercano de casa a seguir trabajando aun en tiempos de pandemia y confinamiento
+
+## Realizado por el amor de :pencil: 
+
+- Laura Rodriguez 
--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const myList = document.querySelector('ul');
+const myRequest = new Request('menu.json');
+
+
+fetch(myRequest)
+  .then(response => response.json())
+  .then(data => {
+    for (const menu of data.menus) {
+      let listItem = document.createElement('li');
+      listItem.appendChild(
+        document.createElement('strong')
+      ).textContent = menu.Precio;
+      listItem.append(
+        `${
+          menu.precio
+        } €`
+      );
+      listItem.appendChild(
+        document.createElement('strong')
+      ).textContent = `${menu.Titulo}`;
+      myList.appendChild(listItem);
+      listItem.appendChild(
+        document.createElement('strong')
+      ).textContent = `${menu.Descripcion}`;
+      myList.appendChild(listItem);
+    }
+  });
+
+
+
 function foodApp () 
 {
     document.getElementById("shopCart").innerHTML = shopNow()
